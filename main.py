18b60b96f72476dbf5b75e3818ccf4af70907b77
nyancodeDesk <nyanko.shootingstar@gmail.com>
2026-05-24 08:54:47 +0900

    changed path expression

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ class MemoWindow:
         else:
             self.win = tk.Toplevel(root)
             self.root_flag = False
-        self.win.iconbitmap("./assets/easysticky.ico")
+        self.win.iconbitmap("assets/easysticky.ico")
         # Config from json file
         self.config = load_config()
         self.win.geometry(
